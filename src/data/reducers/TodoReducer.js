ac948bbb943851a5192f43e95856a3d99cf2155b
Luis Carlos <luiscarlosnors@gmail.com>
2021-11-08 09:21:01 -0300

    feat: Criando Itens.

--- src/data/reducers/TodoReducer.js
+++ src/data/reducers/TodoReducer.js
@@ -1,5 +1,15 @@
 import * as TodoConstants from "../actions/TodoActions";
 
-const TodoReducer = (todoList = [], action) => {};
+const TodoReducer = (todoList = [], action) => {
+  switch (action.type) {
+    case TodoConstants.TODO_CREATE:
+      return [
+        ...todoList,
+        { id: Date.now(), isChecked: false, description: action.description },
+      ];
+    default:
+      return todoList;
+  }
+};
 
 export default TodoReducer;
