2253bdfd2a01ea6cb1c6cf69370a406e209cefde
Luis Carlos <luiscarlosnors@gmail.com>
2022-04-10 03:05:59 -0300

    Redux Thunk: Listando Itens.

--- src/data/actions/TodoActions.js
+++ src/data/actions/TodoActions.js
@@ -1,28 +1,41 @@
-export const TODO_CREATE = "TODO_CREATE";
-export const TODO_UPDATE = "TODO_UPDATE";
-export const TODO_REMOVE = "TODO_REMOVE";
-export const TODO_CLEAR = "TODO_CLEAR";
+import { TodoService } from '../services/TodoService';
+
+export const TODO_LIST = 'TODO_LIST';
+export const TODO_CREATE = 'TODO_CREATE';
+export const TODO_UPDATE = 'TODO_UPDATE';
+export const TODO_REMOVE = 'TODO_REMOVE';
+export const TODO_CLEAR = 'TODO_CLEAR';
+
+export const list = () => {
+    return async (dispatch) => {
+        const todoList = await TodoService.list();
+        dispatch({
+            type: TODO_LIST,
+            todoList,
+        });
+    };
+};
 
 export const create = (description) => {
-  return {
-    type: TODO_CREATE,
-    description,
-  };
+    return {
+        type: TODO_CREATE,
+        description,
+    };
 };
 export const update = (item) => {
-  return {
-    type: TODO_UPDATE,
-    item,
-  };
+    return {
+        type: TODO_UPDATE,
+        item,
+    };
 };
 export const remove = (id) => {
-  return {
-    type: TODO_REMOVE,
-    id,
-  };
+    return {
+        type: TODO_REMOVE,
+        id,
+    };
 };
 export const clear = () => {
-  return {
-    type: TODO_CLEAR,
-  };
+    return {
+        type: TODO_CLEAR,
+    };
 };
--- src/data/reducers/TodoReducer.js
+++ src/data/reducers/TodoReducer.js
@@ -1,35 +1,39 @@
-import * as TodoConstants from "../actions/TodoActions";
+import * as TodoConstants from '../actions/TodoActions';
 
 const TodoReducer = (todoList = [], action) => {
-  switch (action.type) {
-    case TodoConstants.TODO_CREATE:
-      return [
-        ...todoList,
-        {
-          id: Date.now(),
-          isChecked: false,
-          description: action.description,
-        },
-      ];
-    case TodoConstants.TODO_REMOVE:
-      //return todoList.filter(item => item.id !== action.id);
-      const itemIndex = todoList.findIndex((item) => item.id === action.id);
-      return [
-        ...todoList.slice(0, itemIndex),
-        ...todoList.slice(itemIndex + 1),
-      ];
-    case TodoConstants.TODO_UPDATE:
-      return todoList.map((item) => {
-        if (item.id === action.item.id) {
-          return action.item;
-        }
-        return item;
-      });
-    case TodoConstants.TODO_CLEAR:
-      return todoList.filter((item) => !item.isChecked);
-    default:
-      return todoList;
-  }
+    switch (action.type) {
+        case TodoConstants.TODO_LIST:
+            return action.todoList;
+        case TodoConstants.TODO_CREATE:
+            return [
+                ...todoList,
+                {
+                    id: Date.now(),
+                    isChecked: false,
+                    description: action.description,
+                },
+            ];
+        case TodoConstants.TODO_REMOVE:
+            //return todoList.filter(item => item.id !== action.id);
+            const itemIndex = todoList.findIndex(
+                (item) => item.id == action.id
+            );
+            return [
+                ...todoList.slice(0, itemIndex),
+                ...todoList.slice(itemIndex + 1),
+            ];
+        case TodoConstants.TODO_UPDATE:
+            return todoList.map((item) => {
+                if (item.id === action.item.id) {
+                    return action.item;
+                }
+                return item;
+            });
+        case TodoConstants.TODO_CLEAR:
+            return todoList.filter((item) => !item.isChecked);
+        default:
+            return todoList;
+    }
 };
 
 export default TodoReducer;
