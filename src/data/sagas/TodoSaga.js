12a522fa85950bcd1af2630e6e28ca873bd59148
Luis Carlos <luiscarlosnors@gmail.com>
2022-04-10 03:20:36 -0300

    Aplicando Redux-Saga no projeto

---
+++ src/data/sagas/TodoSaga.js
@@ -0,0 +1,9 @@
+import { all } from 'redux-saga/effects';
+
+export function* hello() {
+    console.log('HELLO');
+}
+
+export default function* TodoSaga() {
+    yield all([hello()]);
+}
--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,18 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 import rootReducer from './data/reducers';
+import TodoSaga from './data/sagas/TodoSaga';
 
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(thunkMiddleware, sagaMiddleware)
+);
+
+sagaMiddleware.run(TodoSaga);
 
 ReactDOM.render(
     <Provider store={store}>
